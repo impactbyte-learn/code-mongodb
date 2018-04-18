46c3125b9e99bf228cf3970dec6dd169cbed9dca
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-18 16:32:44 +0700

    Create basic TodoController

---
+++ using-typeorm/src/controller/TodoController.ts
@@ -0,0 +1,21 @@
+export class TodoController {
+  find() {
+    console.log("FIND");
+  }
+
+  insert() {
+    console.log("INSERT");
+  }
+
+  delete() {
+    console.log("DELETE");
+  }
+
+  deleteOne() {
+    console.log("DELETE ONE");
+  }
+
+  updateOne() {
+    console.log("UPDATE ONE");
+  }
+}
