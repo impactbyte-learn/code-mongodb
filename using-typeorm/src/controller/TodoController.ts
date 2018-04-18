fbfec4c6a49a3467a008ef68ef63a44a6881773b
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-18 17:03:15 +0700

    Fix issue with ormconfig

--- using-typeorm/src/controller/TodoController.ts
+++ using-typeorm/src/controller/TodoController.ts
@@ -2,22 +2,28 @@ import { Todo } from "../entity/Todo";
 
 export class TodoController {
   public static async find() {
-    console.log("FIND");
+    console.log("[i] FIND");
+    const todos = await Todo.find();
+    if (!todos) console.log("[i] ERROR");
+    console.log(todos);
   }
 
-  public static async insert() {
-    console.log("INSERT");
+  public static async create() {
+    console.log("[i] CREATE");
+    const todo = await Todo.create({ text: "NEW TEXT" });
+    todo.save();
+    console.log("[i] NEW TODO CREATED", todo);
   }
 
   public static async delete() {
-    console.log("DELETE");
+    console.log("[i] DELETE");
   }
 
   public static async deleteOne() {
-    console.log("DELETE ONE");
+    console.log("[i] DELETE ONE");
   }
 
   public static async updateOne() {
-    console.log("UPDATE ONE");
+    console.log("[i] UPDATE ONE");
   }
 }
--- using-typeorm/src/index.ts
+++ using-typeorm/src/index.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import { TodoController } from "./controller/TodoController";
+import { Todo } from "./entity/Todo";
 
 // connection settings are in the "ormconfig.json" file
 createConnection()
   .then(async connection => {
-    TodoController.find();
+    TodoController.create();
+    // TodoController.find();
   })
   .catch(error => console.log("Error: ", error));
